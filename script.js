9c46cd86f1567d673b18855d30b80af418cfdac8
luthiano0312 <luthiano.penha@aluno.ce.gov.br>
2025-03-30 14:56:28 -0300

    prototipo completo

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let weekday = weekdays[today.getDay()];
 let monthHTML = document.getElementById("month");
 let todayHTML = document.getElementById("today");
 let days = document.getElementsByClassName("day");
-
+// monthHTML.style.backgroundColor = "black";
 
 
 function dayMonth() {
@@ -22,20 +22,31 @@ function dayMonth() {
     if ((yearNum % 4 === 0 && yearNum % 100 !== 0) || (yearNum % 400 === 0)) {
         let months = [31,29,31,30,31,30,31,30,31,30,31,30];
         for (let i = 0; i < 42; i++) {
+            // zera tudo
             days[i].textContent = "";
         }
 
         for (let i = 0; i < months[today.getMonth() + count]; i++) {
+            // printa os dias
             days[firstWeekDayMonthIndex].textContent = "a";
         }
     }else {
         let months = [31,28,31,30,31,30,31,30,31,30,31,30];
         for (let i = 0; i < 42; i++) {
+            // zera tudo
             days[i].textContent = "";
+            days[i].style.backgroundColor = "hsl(0, 0%, 90%)";
+            days[i].style.border = "1px solid hsl(0, 0%, 80%)";
         }
 
         for (let i = 0; i < months[today.getMonth() + count]; i++) {
+            // printa os dias
             days[firstWeekDayMonthIndex + i].textContent = i + 1;
+            days[firstWeekDayMonthIndex + i].style.backgroundColor = "white";
+
+            if (days[firstWeekDayMonthIndex + i].textContent == day && count == 0) {
+                days[firstWeekDayMonthIndex + i].style.border = "1px solid black";
+            }
         }
 
 
@@ -104,6 +115,36 @@ function previousMonth(params) {
     count--;
     nowdate();
 }
+
+function animationLeft() {
+    document.getElementById("calender").classList.remove("animationLeft");
+    document.getElementById("calender").classList.toggle("animationLeft");
+    function animationFinish() {
+        document.getElementById("calender").classList.remove("animationLeft");
+    }
+    setTimeout(animationFinish, 300);
+}
+
+function animationRight() {
+    document.getElementById("calender").classList.remove("animationRight");
+    document.getElementById("calender").classList.toggle("animationRight");
+    function animationFinish() {
+        document.getElementById("calender").classList.remove("animationRight");
+    }
+    setTimeout(animationFinish, 300);
+}
+
+function resetCount() {
+    count = 0;
+    nowdate();
+    dayMonth();
+    if (count > 0) {
+        animationLeft();
+    }else {
+        animationRight();
+    }
+}
+
 nowdate();
 dayMonth()
 todayHTML.textContent =  day + " | " + month;
